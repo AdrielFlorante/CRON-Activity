1360ebde1e3602e333b8516a1e6d832bb5aea921
Adriel Cedrick B. Florante <152822897+AdrielFlorante@users.noreply.github.com>
2025-04-08 21:34:28 +0000

    Fixed the logging to update the x variable by reading from the log_times.txt

--- loghowmanytimes.sh
+++ loghowmanytimes.sh
@@ -1,2 +1,5 @@
-let "x=x+1"
-echo "x" | tee -a log_times.txt
+let "x=$(tail -n 1 ./log_times.txt)"
+x=$((x + 1))
+
+echo "This workflow has been logged by AdrielFlorante this many times:" > log_times.txt
+echo "$x" | tee -a log_times.txt
